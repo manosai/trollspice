fc91befb15646a759756cdc7fa8927564f811f21
David Wang <david@dwang.org>
2012-09-15 22:16:59 -0400

    calibrated, will need to adjust later depending on the person

--- bodysensor.ino
+++ bodysensor.ino
@@ -2,12 +2,13 @@
 // TrollSpice Project
 
 // Initialize Variables Here
-const int deltaVolt = 100;
+const int deltaVolt = 250;
 int sensorValue = 0;
 long int totalValue[6] = {0, 0, 0, 0, 0, 0};
 int baseValue[6] = {-1, -1, -1, -1, -1, -1}; // if base value is -1, then we know it hasn't been set yet
 int peakValue[6] = {-1, -1, -1, -1, -1, -1};
 int prevVal[6] = {0, 0, 0, 0, 0, 0}; // Keep track of what we sent so we don't send too much repetitive data.
+int isOn[6] = {0, 0, 0, 0, 0, 1};
 
 void calibratePins() {
   // initial variables
@@ -53,20 +54,23 @@ void setup() {
 }
 
 
-
-
 void loop() {
+  Serial.println(analogRead(5));
+  // Logic: Go through each pin. If the pin's value crosses a threshold, then send a boolean (only once)
+  // through the USB. Don't spam with multiple 1's. Only show changes.
   
   for(int i = 0; i < 6; i ++) {
     // Data Format: pin (p) + [port #] + [boolean]
-    if (analogRead(i) + 1000 > peakValue[i]) {
-      if (prevVal[i] != 1) { 
-        Serial.println(String("p" + String(i) + "-1")); 
+    if (analogRead(i) > peakValue[i] || analogRead(i) == 1023) {
+      if (prevVal[i] != 1 && isOn[i]) { 
+        Serial.println(String(String(i) + "-1"));
+        Serial.println(String("PEAKVALUE" + String(peakValue[i]))); 
+        //Serial.println(String(analogRead(i)));
         prevVal[i] = 1;
       }
     } else {
-      if (prevVal[i] != 0) {
-        Serial.println(String("p" + String(i) + "-0")); 
+      if (prevVal[i] != 0 && isOn[i]) {
+        Serial.println(String(String(i) + "-0")); 
         prevVal[i] = 0;
       }
     } 
